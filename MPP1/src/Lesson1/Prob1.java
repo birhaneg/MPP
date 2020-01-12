d16fa2c8bc37029f0e12035ff27bcb10e22a5281
hanib <hanib@172.19.144.94>
2020-01-12 01:30:02 -0600

    git testing

---
+++ MPP1/src/Lesson1/Prob1.java
@@ -0,0 +1,6 @@
+package Lesson1;
+public class Prob1 {
+	public static void main(String[] args) {
+		System.out.println("testing");
+	}
+}
---
+++ MPP1/src/Lesson1/Prob2.java
@@ -0,0 +1,7 @@
+package Lesson1;
+
+public class Prob2 {
+	public void show() {
+		System.out.println("prob2.show()");
+	}
+}
